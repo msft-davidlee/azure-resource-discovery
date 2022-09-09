62c040e2e891f885af3844718c652b472b3c81b1
David Lee <leedavid@microsoft.com>
2022-09-09 11:37:45 -0500

    parse manifest file

--- AzureResourceDiscoveryCli/AzureResourceDiscovery/Core/AzurePolicyGenerator.cs
+++ AzureResourceDiscoveryCli/AzureResourceDiscovery/Core/AzurePolicyGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AzureResourceDiscovery.Core
@@ -10,6 +11,7 @@ namespace AzureResourceDiscovery.Core
     {
         public bool GenerateFiles(string filePath)
         {
+            var manifest = JsonSerializer.Deserialize<Manifest>(File.ReadAllText(filePath));
             throw new NotImplementedException();
         }
     }
---
+++ AzureResourceDiscoveryCli/AzureResourceDiscovery/Core/Manifest.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AzureResourceDiscovery.Core
+{
+    public class UniqueResource
+    {
+        [JsonPropertyName("ard-resource-id")]
+        public string? ResourceId { get; set; }
+
+        [JsonPropertyName("resource-group-names")]
+        public List<string>? ResourceGroupNames { get; set; }
+    }
+
+    public class GroupResources
+    {
+        [JsonPropertyName("resource-group-names")]
+        public List<string>? ResourceGroupNames { get; set; }
+
+        [JsonPropertyName("ard-solution-id")]
+        public string? SolutionId { get; set; }
+
+        [JsonPropertyName("ard-environment")]
+        public string? Environment { get; set; }
+
+        [JsonPropertyName("ard-region")]
+        public string? Region { get; set; }
+    }
+
+    public class Manifest
+    {
+        [JsonPropertyName("unique-resources")]
+        public List<UniqueResource>? UniqueResources { get; set; }
+
+        [JsonPropertyName("group-resources")]
+        public List<GroupResources>? GroupResources { get; set; }
+    }
+}
