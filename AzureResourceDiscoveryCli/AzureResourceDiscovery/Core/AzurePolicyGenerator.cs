7fd40ad8cab2d32747a86f6b4e50df569b8fad0e
David Lee <leedavid@microsoft.com>
2022-09-09 10:34:02 -0500

    initial checkin for cli project #1

---
+++ AzureResourceDiscoveryCli/AzureResourceDiscovery/Core/AzurePolicyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureResourceDiscovery.Core
+{
+    public class AzurePolicyGenerator
+    {
+        public bool GenerateFiles(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AzureResourceDiscoveryCli/AzureResourceDiscovery/Program.cs
@@ -0,0 +1,50 @@
+﻿using AzureResourceDiscovery.Core;
+using CommandLine;
+
+namespace AzureResourceDiscovery
+{
+    internal class Program
+    {
+        public class Options
+        {
+            [Option('f', "filepath", Required = true, HelpText = "File path to JSON")]
+            public string? FilePath { get; set; }
+        }
+
+        static int Main(string[] args)
+        {
+            bool hasErrors = false;
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                if (!string.IsNullOrEmpty(o.FilePath) && File.Exists(o.FilePath))
+                {
+                    var gen = new AzurePolicyGenerator();
+
+                    try
+                    {
+                        if (!gen.GenerateFiles(o.FilePath))
+                        {
+                            hasErrors = true;
+                            using TextWriter errorWriter = Console.Error;
+                            errorWriter.WriteLine("Unable to generate Azure Policy(ies)!");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        hasErrors = true;
+                        using TextWriter errorWriter = Console.Error;
+                        errorWriter.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    hasErrors = true;
+                    using TextWriter errorWriter = Console.Error;
+                    errorWriter.WriteLine("Invalid file path!");
+                }
+            });
+
+            return hasErrors ? -1 : 0;
+        }
+    }
+}
