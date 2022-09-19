5adc588f72237598a3df52e9f41e40d53a7fdbc2
David Lee <leedavid@microsoft.com>
2022-09-19 12:10:10 -0500

    use the same mi for assign for remidiate

--- Apply.ps1
+++ Apply.ps1
@@ -3,6 +3,45 @@ $ErrorActionPreference = "Stop"
 $resultsFile = "results.json"
 
 Push-Location .\AzureResourceDiscoveryCli\AzureResourceDiscovery
+
+$groups = az group list --query "[].{Name:name, Id:id}" | ConvertFrom-Json
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to query for resource groups."
+}
+
+$customerManifest = Get-Content ..\..\manifest.json | ConvertFrom-Json
+# Policy assignment requires the use of a managed identity.
+$mi = $customerManifest."managed-identity"
+$miResourceGroup = $mi."resource-group-name"
+$miName = $mi."name"
+
+$exist = $groups | Where-Object { $_.Name -eq $miResourceGroup }
+if (!$exist) {
+
+    # if group does not exist, create it
+    Write-Host "Managed identity resource group $miResourceGroup does not exist. Creating it now..."
+    az group create --name $miResourceGroup --location $customerManifest."resource-group-location" --tags ard-solution-id=ard | ConvertFrom-Json
+    if ($LastExitCode -ne 0) {
+        throw "An error has occured. Unable to create resource group $miResourceGroup."
+    }
+}
+else {
+    Write-Host "Group $miResourceGroup exist"
+}
+
+$ids = az identity list -g $miResourceGroup | ConvertFrom-Json
+if ($LastExitCode -ne 0) {
+    throw "An error has occured. Unable to query for managed identity in resource group $miResourceGroup."
+}
+
+if ($ids.Length -eq 0 -or ($ids | Where-Object { $_.name -eq $miName }).Length -ne 1) {
+    # Create managed identity in resource group
+    $mid = az identity create --name $miName --resource-group $miResourceGroup --tags ard-solution-id=ard | ConvertFrom-Json    
+}
+else {
+    $mid = $ids[0]
+}
+
 dotnet run -- -o $resultsFile -d .\ -f ..\..\manifest.json
 if ($LastExitCode -ne 0) {
     Pop-Location
@@ -16,12 +55,7 @@ if ($LastExitCode -ne 0) {
     throw "An error has occured. Unable to get subscription Id."
 }
 $taggingRoleId = "/subscriptions/$subId/providers/Microsoft.Authorization/roleDefinitions/4a9ae827-6dc8-4573-8ac7-8239d42aa03f"
-$identityName = "tag-policy-identity"
 
-$groups = az group list --query "[].{Name:name, Id:id}" | ConvertFrom-Json
-if ($LastExitCode -ne 0) {
-    throw "An error has occured. Unable to query for resource groups."
-}
 
 foreach ($item in $manifest.Items) {
     
@@ -56,31 +90,18 @@ foreach ($item in $manifest.Items) {
             Write-Host "Group $resourceGroupName exist"
         }
 
-        # Policy assignment requires the use of a managed identity.
-        $ids = az identity list -g $resourceGroupName | ConvertFrom-Json
+        # Perform role assignment
+        az role assignment create --assignee-object-id $mid.principalId --role $taggingRoleId `
+            --resource-group $resourceGroupName --assignee-principal-type ServicePrincipal
         if ($LastExitCode -ne 0) {
-            throw "An error has occured. Unable to query for managed identity in resource group $resourceGroupName."
-        }
-
-        if ($ids.Length -eq 0 -or ($ids | Where-Object { $_.name -eq $identityName }).Length -ne 1) {
-
-            # Create managed identity in resource group
-            $id = az identity create --name $identityName --resource-group $resourceGroupName | ConvertFrom-Json    
-            az role assignment create --assignee-object-id $id.principalId --role $taggingRoleId `
-                --resource-group $resourceGroupName --assignee-principal-type ServicePrincipal
-            if ($LastExitCode -ne 0) {
-                throw "An error has occured. Unable to create managed identity in resource group $resourceGroupName."
-            }
-        }
-        else {
-            $id = $ids[0]
+            throw "An error has occured. Unable to create managed identity in resource group $resourceGroupName."
         }
 
         $policyId = "/subscriptions/$subId/providers/Microsoft.Authorization/policyDefinitions/$name"
         $assignmentName = "$name-assignment"
         $scope = "/subscriptions/$subId/resourceGroups/$resourceGroupName"
         az policy assignment create --name $assignmentName  --scope $scope --policy $policyId `
-            --mi-user-assigned $id.id --location $id.location
+            --mi-user-assigned $mid.id --location $mid.location
             
         if ($LastExitCode -ne 0) {
             throw "An error has occured. Unable to perform policy asssingment in scope of $resourceGroupName."
--- AzureResourceDiscoveryCli/AzureResourceDiscovery/Core/Manifest.cs
+++ AzureResourceDiscoveryCli/AzureResourceDiscovery/Core/Manifest.cs
@@ -2,44 +2,11 @@
 
 namespace AzureResourceDiscovery.Core
 {
-    public class UniqueResource
-    {
-        [JsonPropertyName("name")]
-        public string? Name { get; set; }
-
-        [JsonPropertyName("ard-resource-id")]
-        public string? ResourceId { get; set; }
-
-        [JsonIgnore]
-        public const string TagKey = "ard-resource-id";
-
-        [JsonPropertyName("resource-type")]
-        public string? ResourceType { get; set; }
-
-        [JsonPropertyName("resource-group-names")]
-        public List<string>? ResourceGroupNames { get; set; }
-    }
-
-    public class GroupResources
-    {
-        [JsonPropertyName("name")]
-        public string? Name { get; set; }
-
-        [JsonPropertyName("resource-group-names")]
-        public List<string>? ResourceGroupNames { get; set; }
-
-        [JsonPropertyName(Constants.ArdSolutionId)]
-        public string? SolutionId { get; set; }
-
-        [JsonPropertyName(Constants.ArdEnvironment)]
-        public string? Environment { get; set; }
-
-        [JsonPropertyName(Constants.ArdRegion)]
-        public string? Region { get; set; }
-    }
-
     public class Manifest
     {
+        [JsonPropertyName("managed-identity")]
+        public ManagedIdentityResource? ManagedIdentity { get; set; }
+
         [JsonPropertyName("resource-group-location")]
         public string? ResourceGroupLocation { get; set; }
 
