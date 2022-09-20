a0513bf0cd94316024c1641c64d45e59a006f93d
HaxAway <41404056+HaxAway@users.noreply.github.com>
2022-09-20 16:55:38 -0400

    Added params - create an example container based application so we can demo ARD #5

--- deployment/deployaci.ps1
+++ deployment/deployaci.ps1
@@ -15,4 +15,4 @@ if (!$acr) {
 $AcrName = $acr.Name
 $Password = (az acr credential show -n $AcrName | ConvertFrom-Json).passwords[0].value
 
-az container create -g $ResourceGroupName --name demoapp --image "$AcrName.azurecr.io/demoapp:latest" --registry-password $Password --registry-username $AcrName
+az container create -g $ResourceGroupName --name demoapp --image "$AcrName.azurecr.io/demoapp:latest" --registry-password $Password --registry-username $AcrName --ip-address Public --ports 80
