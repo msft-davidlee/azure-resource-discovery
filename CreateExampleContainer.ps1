c2db5a34458f00c57afa362c591cc2e4e6161f78
HaxAway <41404056+HaxAway@users.noreply.github.com>
2022-09-20 10:52:06 -0400

    Initial commit create an example container based application so we can demo ARD #5

---
+++ CreateExampleContainer.ps1
@@ -0,0 +1,8 @@
+﻿# 1) Connect to Azure. You will be prompted to login. Successful connection returns with the account connected.
+Connect-AzAccount -EnvironmentName AzureUSGovernment
+
+# 2) Create Azure Contrainer Registry, params:
+#    - Resource Group
+#    - Bicep Template File
+#    - Name of the container
+az deployment group create --resource-group rg-cpf-dev-usgovva-001 --template-file AzureContainerRegistry.bicep --parameters acrName=containerRegistryCPF
